a7f7ba6231721fb337804d14d1782d139747c0dc
uttam2797 <uttamnaik2797@gmail.com>
2022-09-17 00:24:05 +0530

    first commit4

---
+++ src/menuItems.js
@@ -0,0 +1,71 @@
+export const menuItems = [
+    {
+      title: 'Home',
+      url: '/',
+      submenu: [
+        {
+            title: 'test1',
+            url: 'test1',
+        },
+        {
+            title: 'test2',
+            url: 'test2',
+        },
+        {
+            title: 'test3',
+            url: 'test3',
+            submenu: [
+                {
+                    title: 'test1',
+                    url: 'test1',
+                },
+                {
+                    title: 'test2',
+                    url: 'test2',
+                },
+                {
+                    title: 'test3',
+                    url: 'test3',
+                    submenu: [
+                        {
+                            title: 'test1',
+                            url: 'test1',
+                        },
+                        {
+                            title: 'test2',
+                            url: 'test2',
+                        },
+                        {
+                            title: 'test3',
+                            url: 'test3',
+                        },
+                    ]
+                },
+            ]
+        },
+      ]
+    },
+    {
+      title: 'Template',
+      url: '/template',
+      submenu: [
+        {
+            title: 'test1',
+            url: 'test1',
+        },
+        {
+            title: 'test2',
+            url: 'test2',
+        },
+        {
+            title: 'test3',
+            url: 'test3',
+        },
+      ]
+    },
+    {
+        title: 'Contact',
+        url: '/contact',
+    }
+  ];
+  
